98f8218dcbde6093dbeeb26d6b74868e899bc729
Janith-Mihiranga <janith.damroit@gmail.com>
2025-04-11 22:12:25 +0530

    UI Upadted

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CounterComponent } from "../counter/counter.component";
 declare var bootstrap: any;
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [CounterComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
