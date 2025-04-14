b4ae5c798cadeed7a9722e2ad34e95d203899cf3
Janith-Mihiranga <janith.damroit@gmail.com>
2025-04-14 23:19:59 +0530

    ch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,25 @@ import {RouterModule } from '@angular/router';
 import { HomePageComponent } from "./home-page/home-page.component";
 import { OurservicessComponent } from "./ourservicess/ourservicess.component";
 
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+declare let gtag: Function; // 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, HomePageComponent, OurservicessComponent],
+  imports: [RouterModule,  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent  {
- 
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      gtag('config', 'G-4YB3RWN6LN', {
+        page_path: event.urlAfterRedirects,
+      });
+    });
+  }
 }
